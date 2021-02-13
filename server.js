f9c6094f081d2d840076a273203a1c2cc8b9b369
Max Lechner <mkonlech@gmail.com>
2021-02-13 14:35:03 -0800

    update information

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-
 require("dotenv").config();
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 // process dynamic data
