7034c6051b8d0bbd1058679d51ec342aae896e14
Max Lechner <mkonlech@gmail.com>
2021-02-13 17:55:16 -0800

    update API routes for testing

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,33 @@
-const express = require("express");
-const router = express.Router();
+const path = require("path");
 const Workout = require("../models/workout.js");
+
+// get workouts
+module.exports = function( app ) {
+
+    app.get("/api/workouts", (req, res) => {
+
+        Workout.find()
+            .then(results => {
+                res.json( results );
+            })
+            .catch(err =>{
+                res.status(500).json(err);
+            });
+
+    });
+
+    app.get("/api/workouts", (req, res) => {
+
+        Workout.create(req.body)
+            .then(results => {
+                res.json( results );
+            })
+            .catch(err =>{
+                res.status(500).json(err);
+            });
+            
+    });
+
+
+
+}
--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,18 @@
 const path = require("path");
 
 module.exports = function(app) {
+
+    // get index page
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+    // get exercise page
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
+    // get stats page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"))
     });
+
 }
